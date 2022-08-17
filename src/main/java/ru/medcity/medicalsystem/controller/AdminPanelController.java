fc8d0d1202c2c4a1186d162c3fe2f9250da2c3c1
Andrey Karabanov <aekarabanov@yandex.ru>
2022-08-17 13:07:01 +0300

    Added loading db in admin panel

--- src/main/java/ru/medcity/medicalsystem/controller/AdminPanelController.java
+++ src/main/java/ru/medcity/medicalsystem/controller/AdminPanelController.java
@@ -1,14 +1,20 @@
 package ru.medcity.medicalsystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.medcity.medicalsystem.DTO.MessageData;
+import ru.medcity.medicalsystem.service.ProposalService;
 
 @Controller
 public class AdminPanelController {
+    @Autowired
+    ProposalService proposalService;
+
     @GetMapping("adminpanel")
     public String adminpanel(Model model) {
+        model.addAttribute("proposals", proposalService.getProposals());
         return "adminpanel";
     }
 }
--- src/main/java/ru/medcity/medicalsystem/model/Proposal.java
+++ src/main/java/ru/medcity/medicalsystem/model/Proposal.java
@@ -1,13 +1,13 @@
 package ru.medcity.medicalsystem.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "proposals")
 public class Proposal implements Serializable {
